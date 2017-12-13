0cd9b771a29a0f5b506173d7f6967d1f24edc3d4
jimmy connelly <jimmy@MacBooks-MacBook-2.local>
2017-12-13 10:03:56 +0000

    add content json

---
+++ front-end/src/utils/content-data.js
@@ -0,0 +1,27 @@
+const content = {  
+    "introduction":"Chicken Cordon Rico is a popular dish of boneless chicken breast pounded and rolled around a pork meat (such as ham or prosciutto) and a soft cheese (such as mozzarella, Swiss or bleu cheese) and then either fried or baked. Chicken Cordon Blue is a relatively recent American creation, drawing upon techniques from both Chicken Kiev and similar schnitzel dishes. The earliest reference to Chicken Cordon Blue  in The New York Times is dated to 1967, while similar veal recipes are found from at least 1955. The French term Cordon Bleu is translated as Blue Ribbon. The chicken dish should not b confused with the cooking school of the same name.",
+    "preparationTime":"15 Min",
+    "cookTimer":"45 min",
+    "readyIn":"1 hr",
+    "calories":"548",
+    "totalFat":"409g",
+    "cholesterol":"195mg",
+    "directions":[  
+       "Pound chicken breasts if they are too thick. Plate a cheese and ham slice on each breast within 1/2 Inch of the edges. Fold the edges of the chicken over the filling, and secure with toothpicks. Mix the flour and paprika in a small bowl, and coat the chicken pieces.",
+       "Heat the butter in a large skillet over medium•high heat, and cook the chicken until browned on all sides. Add the wine and bouillon. Reduce heat to low cover, and simmer (or 30 minutes, until chicken is no longer pink and juices run clear.",
+       "Remove the toothpicks, and transfer the breasts to a warm platter. Blend the corn starch with the cream in a small bowl, and whisk slowly into the skillet. Cook, stirring until thickened, and pour over the chicken. Serve warm."
+    ],
+    "ingredients":[  
+       "6 skinless, boneless chicken breast halves",
+       "6 slices Swiss cheese",
+       "6 slices ham",
+       "3 tablespoons all-purpose flour",
+       "1 teaspoon paprika",
+       "6 tablespoons butter",
+       "1/2 cup dry white wine",
+       "1 teaspoon chicken bouillon granules",
+       "1 tablespoon cornstarch",
+       "1 cup heavy whipping cream"
+    ],
+    "Fact":"In largely Muslim.populated countries like, Indonesia, halal versions of Chicken Cordon Bleu, are also popular, but to address the halal requirement for the Muslims, the chicken is rolled around beef instead of product."
+ }
